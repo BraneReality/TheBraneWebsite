3e1748b13cc0e54a0976d6c6c7543afa0ce212e4
WireWhiz <eli@knisley.info>
2024-11-18 22:17:59 -0500

    Get simple homepage set up

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import braneLogo from '/branelogo.png'
+import './App.css'
+import GitPreview from './components/gitopengraph'
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <img className={"mx-auto logo"} src={braneLogo} alt="Brane Engine Logo" />
+                <h1>Brane Engine</h1>
+                <h2>The framework for building self-hosted multiplayer VR platforms</h2>
+                <br />
+                <p>I'm still working on these sites, so in the meantime check out the projects open source repos!</p>
+                <br />
+                <GitPreview className={"mx-auto overflow-clip rounded-xl m-2 block max-w-[50%]"} owner={"WireWhiz"} repo={"BraneEngine"} />
+                <GitPreview className={"mx-auto overflow-clip rounded-xl m-2 block max-w-[50%]"} owner={"WireWhiz"} repo={"BraneScript"} />
+                <GitPreview className={"mx-auto overflow-clip rounded-xl m-2 block max-w-[50%]"} owner={"WireWhiz"} repo={"TreeSitterBraneScript"} />
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        cors: {
+            origin: ["https://github.com"],
+            credentials: true,
+            preflightContinue: false,
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+        }
+    }
+})
